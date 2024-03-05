0066b14c01b571708d8dff49138f1758b66dd077
MushuTFD <lwyiscool@gmail.com>
2024-03-05 19:47:24 +0800

    update logs

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-module.exports.handler = async (event) => {
+  module.exports.handler = async (event) => {
+    console.log(new Date() + " Second Last Session Of Cohort 4")
     return {
       statusCode: 200,
       body: JSON.stringify(
         {
-          message: "LWY Serverless test",
+          message: "THIS IS OUR HTTP RESPONSE",
         },
         null,
         2
